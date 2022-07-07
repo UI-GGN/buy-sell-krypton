bc06eb6d36e6d6b9d0f25417ec9e591ff6f2e6f6
shubhneetkaur <shubhneet.kaur@thoughtworks.com>
2022-07-07 21:33:46 +0530

    Added register button in HomePage

--- buy-sell/src/Marquee.js
+++ buy-sell/src/Marquee.js
@@ -1,17 +1,12 @@
-import React, {Component} from "react";
-import './marquee.css';
+import React from "react";
+import "./marquee.css";
 
-
-class Marquee extends Component
-{
-    render()
-    {
-        return(
-            <div class="blink">
-                    <marquee >!!! products on discount !!! Incredible Deals !!! Most Loved Brands !!!</marquee>
-            </div>
-        )
-    }
+function Marquee() {
+  return (
+    <div class="blink">
+      <marquee>!!! products on discount !!! Incredible Deals !!! Most Loved Brands !!!</marquee>
+    </div>
+  );
 }
 
 export default Marquee;
--- buy-sell/src/NavBar.js
+++ buy-sell/src/NavBar.js
@@ -1,33 +1,41 @@
-import React, {Component} from "react";
-import './NavBar.css';
+import React, { useState } from "react";
+import "./NavBar.css";
+import PopUp from "./components/PopUp";
 
+function NavBar() {
+  const [buttonPopup, setButtonPopup] = useState(false);
+  return (
+    <header className="top-section-layout">
+      <div className="top-nav">
+        <a className="active" href="#home">
+          Home
+        </a>
+        <a href="#about">About</a>
+        <a href="#productsList">ProductsList</a>
 
-
-class NavBar extends Component
-{
-    render()
-    {
-        return(
-            <header className="top-section-layout">
-                <div className="top-nav">
-                    <a className="active" href="#home">Home</a>
-                    <a href="#about">About</a>
-                    <a href="#productsList">ProductsList</a>
-                    <div className="search-container">
-                        <form>
-                        <img src="https://cdn-icons.flaticon.com/png/512/2838/premium/2838838.png?token=exp=1657127010~hmac=eed065251fd9317fbbd01eb84e8f2383" alt="cart"></img>
-                        <button type="submit">Submit</button>
-                        <input type="text" placeholder="Search.." name="search"></input>
-                        
-                        </form>
-                    </div>
-                </div>
-            </header>
-
-    
-
-        )
-    }
-
+        <div className="search-container">
+            <button
+              className="registartionButton"
+              onClick={() => setButtonPopup(true)}
+            >
+              {" "}
+              Register here
+            </button>
+          {/* <div>
+            <form>
+              <img
+                src="https://cdn-icons.flaticon.com/png/512/2838/premium/2838838.png?token=exp=1657127010~hmac=eed065251fd9317fbbd01eb84e8f2383"
+                alt="cart"
+              ></img>
+              <button type="submit">Submit</button>
+              <input type="text" placeholder="Search.." name="search"></input>
+            </form>
+          </div> */}
+          <PopUp trigger={buttonPopup} setTrigger={setButtonPopup}></PopUp>
+        </div>
+      </div>
+    </header>
+  );
 }
-export default NavBar
+
+export default NavBar;
