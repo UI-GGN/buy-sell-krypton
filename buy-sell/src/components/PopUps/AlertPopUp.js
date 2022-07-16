d7615ad11d57751eee0553d0e511efec81d73dc6
shubhneetkaur <shubhneet.kaur@thoughtworks.com>
2022-07-16 12:03:35 +0530

    Add IconButton for close button

--- buy-sell/src/components/PopUps/AlertPopUp.js
+++ buy-sell/src/components/PopUps/AlertPopUp.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./AlertPopUp.css";
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 function AlertPopUp(props) {
   return props.trigger ? (
     <div className="alertPopUp">
         <div className="alertPopUp-inner">
-      <button className="close-btn" onClick={() => props.setTrigger(false)}>
-        {" "}
-        Close
-      </button>
-      {props.title}
+      <IconButton className="close-btn" aria-label="close" onClick={() => props.setTrigger(false)}>
+      <CloseIcon />
+    </IconButton>
+      <strong>{props.title}</strong>
     </div>
     </div>
   ) : (
