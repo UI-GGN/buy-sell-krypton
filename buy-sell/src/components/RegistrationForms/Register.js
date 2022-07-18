db825fe6930bf185636ec62ab2ea5ddf8cc6d315
shubhneetkaur <shubhneet.kaur@thoughtworks.com>
2022-07-18 13:09:09 +0530

    Update folder name and minor refactoring

--- buy-sell/src/components/RegistrationForms/Register.js
+++ buy-sell/src/components/RegistrationForms/Register.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { FormikProvider, useFormik } from "formik";
 import "./Register.css";
-import AlertPopUp from "../PopUps/AlertPopUp";
+import AlertPopUp from "../Modal/AlertPopUp";
 import NavBar from "../../NavBar";
 
 const validateData = (data) => {
@@ -53,104 +53,109 @@ function Register() {
     validate: validateData,
     validateOnMount: true,
     onSubmit: () => {
-      localStorage.setItem(formik.values.username, JSON.stringify({"password":formik.values.password, "role": "buyer"}))
+      localStorage.setItem(
+        formik.values.username,
+        JSON.stringify({ password: formik.values.password, role: "buyer" })
+      );
       var retrievedObject = localStorage.getItem(formik.values.username);
 
-      console.log('retrievedObject: ', retrievedObject);
+      console.log("retrievedObject: ", retrievedObject);
       setButtonPopup(true);
     },
   });
 
   return (
-    <div className="Registration">
+    <div>
       <NavBar />
-      <h1> Welcome to Buyer Registration Page </h1>
-      <FormikProvider value={formik}>
-        <form onSubmit={formik.handleSubmit}>
-          <p>
-            <label htmlFor="username">
-              <strong>Username:</strong>
-              <input
-                type="text"
-                name="username"
-                value={formik.values.username}
-                onChange={formik.handleChange}
-              />
-              <br />
-              {formik.values.username && formik.errors.username ? (
-                <h5 style={{ color: "red", paddingLeft: "160px" }}>
-                  {formik.errors.username}
-                </h5>
-              ) : null}
-            </label>{" "}
-          </p>
-          <p>
-            <label htmlFor="phoneNumber">
-              <strong>Phone number:</strong>
-              <input
-                type="text"
-                name="phoneNumber"
-                value={formik.values.phoneNumber}
-                onChange={formik.handleChange}
-              />{" "}
-              {formik.values.phoneNumber && formik.errors.phoneNumber ? (
-                <h5 style={{ color: "red", paddingLeft: "160px" }}>
-                  {formik.errors.phoneNumber}
-                </h5>
-              ) : null}
-            </label>
-          </p>
-          <p>
-            <label htmlFor="password">
-              <strong>Password:</strong>
-              <input
-                type="password"
-                name="password"
-                value={formik.values.password}
-                onChange={formik.handleChange}
-              />
-              <br />
-              {formik.values.password &&
-              formik.values &&
-              formik.errors.password ? (
-                <h5 style={{ color: "red", paddingLeft: "160px" }}>
-                  {formik.errors.password}
-                </h5>
-              ) : null}
-            </label>
-          </p>
-          <p>
-            <label htmlFor="confirmPassword">
-              <strong>Confirm password:</strong>
-              <input
-                type="password"
-                name="confirmPassword"
-                value={formik.values.confirmPassword}
-                onChange={formik.handleChange}
-              />
-              <br />
-              {formik.values.confirmPassword &&
-              formik.errors.confirmPassword ? (
-                <h5 style={{ color: "red", paddingLeft: "160px" }}>
-                  {formik.errors.confirmPassword}
-                </h5>
-              ) : null}
-            </label>
-          </p>
-          <button
-            type="submit"
-            style={{ marginLeft: "180px" }}
-            disabled={!(formik.isValid && formik.dirty)}
-          >
-            Submit{" "}
-          </button>
-        </form>
-      </FormikProvider>
-      <AlertPopUp
-        trigger={buttonPopup}
-        setTrigger={setButtonPopup}
-        title={"Buyer Successfully Registered!!"}
-      ></AlertPopUp>
+      <div className="registration">
+        <h1> Welcome to Buyer Registration Page </h1>
+        <FormikProvider value={formik}>
+          <form onSubmit={formik.handleSubmit}>
+            <p>
+              <label htmlFor="username">
+                <strong>Username:</strong>
+                <input
+                  type="text"
+                  name="username"
+                  value={formik.values.username}
+                  onChange={formik.handleChange}
+                />
+                <br />
+                {formik.values.username && formik.errors.username ? (
+                  <h5 style={{ color: "red", paddingLeft: "160px" }}>
+                    {formik.errors.username}
+                  </h5>
+                ) : null}
+              </label>{" "}
+            </p>
+            <p>
+              <label htmlFor="phoneNumber">
+                <strong>Phone number:</strong>
+                <input
+                  type="text"
+                  name="phoneNumber"
+                  value={formik.values.phoneNumber}
+                  onChange={formik.handleChange}
+                />{" "}
+                {formik.values.phoneNumber && formik.errors.phoneNumber ? (
+                  <h5 style={{ color: "red", paddingLeft: "160px" }}>
+                    {formik.errors.phoneNumber}
+                  </h5>
+                ) : null}
+              </label>
+            </p>
+            <p>
+              <label htmlFor="password">
+                <strong>Password:</strong>
+                <input
+                  type="password"
+                  name="password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                />
+                <br />
+                {formik.values.password &&
+                formik.values &&
+                formik.errors.password ? (
+                  <h5 style={{ color: "red", paddingLeft: "160px" }}>
+                    {formik.errors.password}
+                  </h5>
+                ) : null}
+              </label>
+            </p>
+            <p>
+              <label htmlFor="confirmPassword">
+                <strong>Confirm password:</strong>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  value={formik.values.confirmPassword}
+                  onChange={formik.handleChange}
+                />
+                <br />
+                {formik.values.confirmPassword &&
+                formik.errors.confirmPassword ? (
+                  <h5 style={{ color: "red", paddingLeft: "160px" }}>
+                    {formik.errors.confirmPassword}
+                  </h5>
+                ) : null}
+              </label>
+            </p>
+            <button
+              type="submit"
+              style={{ marginLeft: "180px" }}
+              disabled={!(formik.isValid && formik.dirty)}
+            >
+              Submit{" "}
+            </button>
+          </form>
+        </FormikProvider>
+        <AlertPopUp
+          trigger={buttonPopup}
+          setTrigger={setButtonPopup}
+          title={"Buyer Successfully Registered!!"}
+        ></AlertPopUp>
+      </div>
     </div>
   );
 }
